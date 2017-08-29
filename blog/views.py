25ffff9e4b3e597d08ba50eb29c11a15d46f104a
CoriAle <1995coral@hotmail.es>
2017-08-29 11:49:31 -0600

    Ng for en django

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Publicacion
+from django.utils import timezone
 
 # Create your views here.
 def listar_pub(request):
-    return render(request, 'blog/listar_pub.html',{})
+    pub = Publicacion.objects.filter(fecha_publicacion__lte = timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar_pub.html',{'pub': pub})
